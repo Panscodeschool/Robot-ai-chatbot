c753f00361c55c20dc256066d4dac93d4e0e8da2
Panda Tran <106720740+giahuytrannguyen@users.noreply.github.com>
2022-07-28 11:37:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+print('Hello')
+print('This is the passcode')
+print('might be 1234')
+passcodeGuess = input('What is the passcode? ')
+print('Access Granted!')
+Passcode = '1234'
+stats = "Welcome to chatbot!"
+fakestats = ' You Can you trust might be 1234 hahahaha Trust problem #lol'
+def passcode():
+  if passcodeGuess == Passcode:
+    print('System stats = '+ stats )
+  else:
+    print('access denied')
+    print('stats:' + fakestats)
+    quit('BADD')
+  
+  
+    
+  
+
+passcode()
+ChatBot_Name = "chat_bot1"
+print('commands are: Who are you?,Hi,Quit,what version is it?,I am sad,why,Your not real')
+UserInput = input("You: ")
+
+if UserInput == 'Who are you?':
+  print('ChatBot1: I am ' + ChatBot_Name)
+  UserInput = input("You: ")
+if UserInput == 'Hi':
+  print('ChatBot1: Hi')
+  UserInput = input('You: ') 
+if UserInput == "quit":
+  print("Chatbot: goodbye")
+  quit("Chatbot left")
+if UserInput == "what version is it?":
+  print("ChatBot: version 4.5 beta")
+  UserInput = input('You: ')
+if UserInput == 'I am sad':
+ print('Chatbot: That is sad')
+ UserInput = input('You: ')
+if UserInput == 'why':
+    print('ChatBot: idk')
+    UserInput = input('You: ')
+if UserInput == 'But why?':
+  print('Chatbot: Ok i was just made because The bot dev was bored like this one')
+  UserInput = input('You: ')
+if UserInput == 'Your not real':
+  print('ChatBot: Hey, I am a robot I am real what did I do? to get this')
+  UserInput == input('bad:')
+
+  print('Hello')
+  print('This is the passcode')
+  print('might be 074#')
+  passcodeGuess = input('What is the passcode? ')
+  print('Access Granted!')
+  Passcode = '0748'
+  stats = "Welcome to Dev layer"
+  fakestats = ' You shal not pass!'
+  def passcode():
+   if passcodeGuess == Passcode:
+    print('System stats = '+ stats )
+   else:
+    print('access denied')
+    print('stats:' + fakestats)
+    exit('Hahaha, acces denied')
+
+  
+
+  passcode()
+Dev1 = input('Dev: ')
+if Dev1 == 'User':
+  print('Botchat: Ok')
+  UserInput = input('You: ')
+if Dev1 == 'Help':
+  print('Chatbot: I can not do that')
+  Dev1 = input('Dev: ')
+if Dev1 == 'Boot Error':
+  print('Chatbot: Ok...')
+  while True:
+   print('Bad idea!!!!')
+   print('Noooooo')
+   print('Shutdown in..')
+   print('Error')
+   
+if Dev1 == 'restart':
+  print('Chatbot: I can stop program do you want to?')
+  YesOrNo = input('Dev: ')
+if Dev1 == 'yes':
+  print('Chatbot: Ok!')
+  quit('Dev said to stop')
+else:
+  print('Chatbot: Ok I wont do it!')
+  Dev1 = input('Dev: ')
+  
+
+
+  
+  
